5a236e382f5e56c0dd0fcbf1290d7ae7bed69f43
root <root@host.localdomain>
2019-06-14 04:34:24 -0400

    Server_ ServerBuild 1.0.2 archieve

---
+++ Release_osx.sh
@@ -0,0 +1,19 @@
+if [ -n "$1" ]; then
+    echo "归档平台: osx"
+    echo "归档版本号: $1"
+    build_dir="/root/webserver/BiangStudio/wp-content/themes/BiangTheme/Projects/MechStorm/Build/$1"
+    echo "输出路径: $build_dir"
+    if [ ! -d "$build_dir" ];then
+      mkdir $build_dir
+      echo "路径不存在，自动创建"
+    fi
+    zip_name="$build_dir/MechStorm-MacOS-V$1.app.zip"
+    echo "归档压缩包名称: $zip_name"
+    if [ ! -f "$zip_name" ];then
+      zip -r $zip_name /root/GameServer/MyFirstCardGame/Client/Build/osx/*
+    else
+      echo "已存在该版本zip，取消归档"
+    fi
+else
+    echo "请使用版本号作第一参数"
+fi
