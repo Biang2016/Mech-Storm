5f30175f7620f7000443d6df53bb7c1d134442f4
root <root@host.localdomain>
2019-12-05 23:23:11 -0500

    serverbuild

---
+++ Release_osxTestServer.sh
@@ -0,0 +1,21 @@
+if [ -n "$1" ]; then
+    echo "归档平台: osx"
+    echo "归档版本号: $1"
+    build_dir="/root/webserver/BiangStudio/wp-content/themes/BiangTheme/Projects/MechStorm/Build/$1"
+    echo "输出路径: $build_dir"
+    if [ ! -d "$build_dir" ];then
+      mkdir $build_dir
+      echo "路径不存在，自动创建"
+    fi
+    zip_name="$build_dir/MechStorm-TestServer-MacOS-V$1.app.zip"
+    echo "归档压缩包名称: $zip_name"
+    if [ ! -f "$zip_name" ];then
+      cd /root/GameServer/MechStorm/Client/Build/localServer/osx/
+      chmod +x ./MechStorm_LocalServer.app/Contents/MacOS/MechStorm_LocalServer
+      zip -r $zip_name ./*
+    else
+      echo "已存在该版本zip，取消归档"
+    fi
+else
+    echo "请使用版本号作第一参数"
+fi
